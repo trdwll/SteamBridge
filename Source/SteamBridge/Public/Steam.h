320adc394bee4f97afb28fbe3e209081b7d3635f
Russ Treadwell <git@trdwll.com>
2020-11-11 16:53:30 -0500

    disable warnings about strncpy etc

--- Source/SteamBridge/Public/Steam.h
+++ Source/SteamBridge/Public/Steam.h
@@ -1,6 +1,16 @@
 // Copyright 2020 Russ 'trdwll' Treadwell <trdwll.com>. All Rights Reserved.
 #pragma once
 
+#ifdef _MSC_VER
+#pragma warning(push)
+#pragma warning(disable:4996)
+#pragma warning(disable:4828)
+#pragma warning(disable:4265)
+#endif
+
 #include "ThirdParty/Steamworks/Steamv147/sdk/public/steam/steam_api.h"
 #include "ThirdParty/Steamworks/Steamv147/sdk/public/steam/steam_gameserver.h"
 
+#ifdef _MSC_VER
+#pragma warning(pop)
+#endif
