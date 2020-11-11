a401e1a013770f9de92d4d60e05e81ea0e070c75
Russ Treadwell <git@trdwll.com>
2020-11-11 18:12:47 -0500

    start working on ISteamHTTP

---
+++ Source/SteamBridge/Private/Core/SteamHTTP.cpp
@@ -0,0 +1,15 @@
+// Copyright 2020 Russ 'trdwll' Treadwell <trdwll.com>. All Rights Reserved.
+
+#pragma once
+
+#include "Core/SteamHTTP.h"
+
+#include "SteamBridgeUtils.h"
+
+USteamHTTP::USteamHTTP()
+{
+}
+
+USteamHTTP::~USteamHTTP()
+{
+}
