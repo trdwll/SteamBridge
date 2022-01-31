09db82f2b4a9dd54d181d97f04daf8a2d8c91682
Russ Treadwell <git@trdwll.com>
2022-01-30 23:11:16 -0500

    update copyright year

--- Source/SteamBridge/Private/Core/SteamUtils.cpp
+++ Source/SteamBridge/Private/Core/SteamUtils.cpp
@@ -1,4 +1,4 @@
-// Copyright 2020-2021 Russ 'trdwll' Treadwell <trdwll.com>. All Rights Reserved.
+// Copyright 2020-2022 Russ 'trdwll' Treadwell <trdwll.com>. All Rights Reserved.
 
 #include "Core/SteamUtils.h"
 
